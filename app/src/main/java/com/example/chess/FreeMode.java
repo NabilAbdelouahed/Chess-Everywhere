a3353d1199ecda965c15d20505214f47dc75ff13
abdelouahed nabil <nabil.abdelouahed@student-cs.fr>
2024-01-16 14:00:00 +0100

    free mode completed

--- app/src/main/java/com/example/chess/FreeMode.java
+++ app/src/main/java/com/example/chess/FreeMode.java
@@ -10,27 +10,53 @@ import android.widget.ImageButton;
 public class FreeMode extends AppCompatActivity {
 
     public boolean isFirstClick = true;
-
+    public String start;
+    public boolean move;
     public Drawable buttonDrawable;
     public ImageButton clickedButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_free_mode);
-
     }
     public void show_moves(View view) {
+        String id = getResources().getResourceEntryName(view.getId());
         clickedButton = (ImageButton) view;
-        if (isFirstClick) {
+        if (isFirstClick && id.charAt(0)=='s') {
             buttonDrawable = clickedButton.getDrawable();
             if (buttonDrawable != null) {
                 isFirstClick = false;
             }
-        } else {
+        } else if(isFirstClick == false && id.charAt(0)=='b' && move == false) {
             if (buttonDrawable != null) {
                 clickedButton.setImageDrawable(buttonDrawable);
             }
             isFirstClick = true;
         }
+        else if(isFirstClick && id.charAt(0)=='b'){
+            buttonDrawable = clickedButton.getDrawable();
+            if (buttonDrawable != null) {
+                isFirstClick = false;
+                start = id;
+                move = true;
+            }
+        }
+        else if(isFirstClick == false && id.charAt(0)=='b' && move == true) {
+            if (buttonDrawable != null) {
+                removeButtonImage(start);
+                clickedButton.setImageDrawable(buttonDrawable);
+            }
+            isFirstClick = true;
+            move = false;
+
+        }
     }
+    public void removeButtonImage(String buttonIdString) {
+        int resID = getResources().getIdentifier(buttonIdString, "id", getPackageName());
+        ImageButton button = findViewById(resID);
+        if (button != null) {
+            button.setImageResource(0); // Removes the image
+        }
+    }
+
 }
