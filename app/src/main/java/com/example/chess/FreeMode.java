3157e1df35667d8fec3d4087fc9a1722186e8e60
abdelouahed nabil <nabil.abdelouahed@student-cs.fr>
2024-02-12 20:55:16 +0100

    Free mode show selected piece added Opening files updated

--- app/src/main/java/com/example/chess/FreeMode.java
+++ app/src/main/java/com/example/chess/FreeMode.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class FreeMode extends AppCompatActivity {
@@ -14,6 +15,10 @@ public class FreeMode extends AppCompatActivity {
     public boolean move;
     public Drawable buttonDrawable;
     public ImageButton clickedButton;
+    private Drawable tempDrawable;
+    private int tilePositionSum;
+    private View lastClickedButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,18 +28,32 @@ public class FreeMode extends AppCompatActivity {
         String id = getResources().getResourceEntryName(view.getId());
         clickedButton = (ImageButton) view;
         if (isFirstClick && id.charAt(0)=='s') {
+            lastClickedButton = view;
+            tempDrawable = view.getBackground();
+            view.setBackground(getDrawable(R.drawable.white_tile_lighted));
             buttonDrawable = clickedButton.getDrawable();
             if (buttonDrawable != null) {
                 isFirstClick = false;
             }
-        } else if(isFirstClick == false && id.charAt(0)=='b' && move == false) {
+        }
+        else if(isFirstClick == false && id.charAt(0)=='b' && move == false) {
             if (buttonDrawable != null) {
                 clickedButton.setImageDrawable(buttonDrawable);
             }
+            lastClickedButton.setBackground(tempDrawable);
             isFirstClick = true;
         }
-        else if(isFirstClick && id.charAt(0)=='b'){
+        else if(isFirstClick && id.charAt(0)=='b' && ((ImageButton) view).getDrawable() != null){
             buttonDrawable = clickedButton.getDrawable();
+            lastClickedButton = view;
+            tempDrawable = view.getBackground();
+            tilePositionSum = (int) (Integer.valueOf(id.substring(id.length() - 2)) % 10 )+ (Integer.valueOf(id.substring(id.length() - 2)) / 10) ;
+            if (tilePositionSum % 2 == 0){
+                view.setBackground(getDrawable(R.drawable.green_tile_lighted));
+            }
+            else{
+                view.setBackground(getDrawable(R.drawable.white_tile_lighted));
+            }
             if (buttonDrawable != null) {
                 isFirstClick = false;
                 start = id;
@@ -46,8 +65,16 @@ public class FreeMode extends AppCompatActivity {
                 removeButtonImage(start);
                 clickedButton.setImageDrawable(buttonDrawable);
             }
+            lastClickedButton.setBackground(tempDrawable);
             isFirstClick = true;
             move = false;
+
+        }
+        else if(!isFirstClick && id.charAt(0)=='s' && getResources().getResourceEntryName(lastClickedButton.getId()).charAt(0) == 's'){
+            lastClickedButton.setBackground(getDrawable(R.drawable.white_tile));
+            buttonDrawable = clickedButton.getDrawable();
+            view.setBackground(getDrawable(R.drawable.white_tile_lighted));
+            lastClickedButton =  view;
 
         }
     }
